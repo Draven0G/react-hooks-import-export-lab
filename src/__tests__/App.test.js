9648147d64d82ef92ed1db37a07bb1e831b82274
Ramsey <browenbroz@gmail.com>
2024-04-23 11:04:21 +0300

    w2la

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,6 +2,7 @@ import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
 import App from "../components/App";
 
+
 test("it renders without crashing", () => {
   try {
     expect(() => render(<App />)).not.toThrow();
@@ -9,3 +10,4 @@ test("it renders without crashing", () => {
     throw new Error("Make sure to import the required components!");
   }
 });
+
